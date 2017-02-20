1f359e3df9a66fc24d92fd0d5f290012a5b488ee
Alejandro <Alejandro>
2017-02-20 17:19:10 +0100

    Practicas Lunes

--- tareas.js
+++ tareas.js
@@ -1,13 +1,25 @@
 
-
 class MainController {
 
-    
+    constructor () {
+        // Constructor Vacio
+    }
 
+    $onInit () {
+        console.info("Iniciado Correctamente");
+        this.aTareas = [{nombre : "Sacar la Basura", isHecho : false}];        
+    }
+
+    addTarea() {        
+        this.aTareas.push({nombre : this.nuevaTarea, isHecho : false});
+        this.nuevaTarea = '';
+    }
+
+    borrarValor(valor){
+        this.aTareas.splice(valor);
+    }
 
 }
-
-
 
 angular.module('appTareas',[])
 .controller('MainController', MainController);
